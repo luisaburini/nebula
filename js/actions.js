cd831b0fa1921cb2d253508a409a2d9f079943c0
Luisa Burini <luisa.burini@gmail.com>
2021-05-24 22:10:39 -0300

    :sparkles: Adicionando videos

---
+++ js/actions.js
@@ -0,0 +1,189 @@
+function onEndedAudio01pt() 
+{
+    console.log("AUDIO ENDED 01")
+    var texto01pt = document.getElementById("texto01pt");
+    texto01pt.style.display = "block";
+    var audio01pt = document.getElementById("audio01pt");
+    audio01pt.loop = "true";
+    audio01pt.play();
+}
+
+function onEndedAudio02pt() 
+{
+    console.log("AUDIO ENDED 02")
+    var texto02pt = document.getElementById("texto02pt");
+    texto02pt.style.display = "block";
+    var audio02pt = document.getElementById("audio02pt");
+    audio02pt.loop = "true";
+    audio02pt.play();
+}
+
+function onEndedAudio03pt() 
+{
+    console.log("AUDIO ENDED 03")
+    var texto03pt = document.getElementById("texto03pt");
+    texto03pt.style.display = "block";
+    var audio03pt = document.getElementById("audio03pt");
+    audio03pt.loop = "true";
+    audio03pt.play();
+}
+
+function onEndedAudio04pt() 
+{
+    console.log("AUDIO ENDED 04")
+    var texto04pt = document.getElementById("texto04pt");
+    texto04pt.style.display = "block";
+    var audio04pt = document.getElementById("audio04pt");
+    audio04pt.loop = "true";
+    audio04pt.play();
+}
+
+function onEndedAudio05pt() 
+{
+    console.log("AUDIO ENDED 05")
+    var texto05pt = document.getElementById("texto05pt");
+    texto05pt.style.display = "block";
+    var audio05pt = document.getElementById("audio05pt");
+    audio05pt.loop = "true";
+    audio05pt.play();
+}
+
+function onEndedAudio06pt() 
+{
+    console.log("AUDIO ENDED 06")
+    var texto06pt = document.getElementById("texto06pt");
+    texto06pt.style.display = "block";
+    var audio06pt = document.getElementById("audio06pt");
+    audio06pt.loop = "true";
+    audio06pt.play();
+}
+
+function onEndedAudio07pt() 
+{
+    console.log("AUDIO ENDED 07")
+    var texto07pt = document.getElementById("texto07pt");
+    texto07pt.style.display = "block";
+    var audio07pt = document.getElementById("audio07pt");
+    audio07pt.loop = "true";
+    audio07pt.play();
+}
+
+function onEndedAudio08pt() 
+{
+    console.log("AUDIO ENDED 08")
+    var texto08pt = document.getElementById("texto08pt");
+    texto08pt.style.display = "block";
+    var audio08pt = document.getElementById("audio08pt");
+    audio08pt.loop = "true";
+    audio08pt.play();
+}
+
+function onEndedAudio09pt() 
+{
+    console.log("AUDIO ENDED 09")
+    var texto09pt = document.getElementById("texto09pt");
+    texto09pt.style.display = "block";
+    var audio09pt = document.getElementById("audio09pt");
+    audio09pt.loop = "true";
+    audio09pt.play();
+}
+
+function onEndedAudio10pt() 
+{
+    console.log("AUDIO ENDED 10")
+    var texto10pt = document.getElementById("texto10pt");
+    texto10pt.style.display = "block";
+    var audio10pt = document.getElementById("audio10pt");
+    audio10pt.loop = "true";
+    audio10pt.play();
+}
+
+function onEndedAudio11pt() 
+{
+    console.log("AUDIO ENDED 11")
+    var texto11pt = document.getElementById("texto11pt");
+    texto11pt.style.display = "block";
+    var audio11pt = document.getElementById("audio11pt");
+    audio11pt.loop = "true";
+    audio11pt.play();
+}
+
+function onEndedAudio12pt() 
+{
+    console.log("AUDIO ENDED 12")
+    var texto12pt = document.getElementById("texto12pt");
+    texto12pt.style.display = "block";
+    var audio12pt = document.getElementById("audio12pt");
+    audio12pt.loop = "true";
+    audio12pt.play();
+}
+
+function onEndedAudio13pt() 
+{
+    console.log("AUDIO ENDED 13")
+    var texto13pt = document.getElementById("texto13pt");
+    texto13pt.style.display = "block";
+    var audio13pt = document.getElementById("audio13pt");
+    audio13pt.loop = "true";
+    audio13pt.play();
+}
+
+function onEndedAudio14pt() 
+{
+    console.log("AUDIO ENDED 14")
+    var texto14pt = document.getElementById("texto14pt");
+    texto14pt.style.display = "block";
+    var audio14pt = document.getElementById("audio14pt");
+    audio14pt.loop = "true";
+    audio14pt.play();
+}
+
+function onEndedAudio15pt() 
+{
+    console.log("AUDIO ENDED 15")
+    var texto15pt = document.getElementById("texto15pt");
+    texto15pt.style.display = "block";
+    var audio15pt = document.getElementById("audio15pt");
+    audio15pt.loop = "true";
+    audio15pt.play();
+}
+
+function onEndedAudio16pt() 
+{
+    console.log("AUDIO ENDED 16")
+    var texto16pt = document.getElementById("texto16pt");
+    texto16pt.style.display = "block";
+    var audio16pt = document.getElementById("audio16pt");
+    audio16pt.loop = "true";
+    audio16pt.play();
+}
+
+function onEndedAudio17pt() 
+{
+    console.log("AUDIO ENDED 17")
+    var texto17pt = document.getElementById("texto17pt");
+    texto17pt.style.display = "block";
+    var audio17pt = document.getElementById("audio17pt");
+    audio17pt.loop = "true";
+    audio17pt.play();
+}
+
+function onEndedAudio18pt() 
+{
+    console.log("AUDIO ENDED 18")
+    var texto18pt = document.getElementById("texto18pt");
+    texto18pt.style.display = "block";
+    var audio18pt = document.getElementById("audio18pt");
+    audio18pt.loop = "true";
+    audio18pt.play();
+}
+
+function onEndedAudio19pt() 
+{
+    console.log("AUDIO ENDED 19")
+    var texto19pt = document.getElementById("texto19pt");
+    texto19pt.style.display = "block";
+    var audio19pt = document.getElementById("audio19pt");
+    audio19pt.loop = "true";
+    audio19pt.play();
+}
